1140d450d0f25b86e9ff4877757e858b6281a4b1
Abdulsamad <almuhandith1497@gmail.com>
2023-04-24 00:00:58 +0100

    Task 3 and 4

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def indexPage():
     """ Function called with / """
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-

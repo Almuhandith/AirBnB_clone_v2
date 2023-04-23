facaeadc322b0386f0cd18c19a9677e1dd35f8ce
Abdulsamad <almuhandith1497@gmail.com>
2023-04-23 23:53:06 +0100

    flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Script that runs an app with Flask framework """
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def indexPage():
+    """ Function called with / """
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
